168cc2a580e17804e55bc97eeb202d8d22aa2ded
marcopetry <marcomattospetry@gmail.com>
2025-02-17 19:39:35 -0300

    feat: add react query to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from "react";
 import { ThemeProvider, CssBaseline, Container } from "@mui/material";
 import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container>{children}</Container>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container>{children}</Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
