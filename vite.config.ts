f2c8991709e358d1d37b11880f9976da0d02b579
marcopetry <marcomattospetry@gmail.com>
2025-02-18 13:15:06 -0300

    build: config vercel to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+  server: {
+    port: 4000,
+  },
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), vercel()],
+  appType: "spa",
 });
